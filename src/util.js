aa0d8441efc214b8c6cbbf9ce6223e11f48fad0e
scan3ls <scan3ls@gmail.com>
2021-07-15 13:23:59 -0500

    Add API url comment for future

--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 function request(params) {
-    const base_url = 'http://127.0.0.1:5000'
+    // const base_url = 'https://salty-oasis-19252.herokuapp.com';
+    const base_url = 'http://127.0.0.1:5000/';
     const endpoints = ['albums', 'artists', 'songs'];
     let url;
 
