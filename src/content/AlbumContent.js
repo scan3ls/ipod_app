1bb200a280872f41583877e74a2695647155deef
scan3ls <scan3ls@gmail.com>
2021-07-20 14:25:07 -0500

    Add place holder components

---
+++ src/content/AlbumContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+class AlbumContent extends React.Component {
+    render() {
+        return (
+            <Text>Albums</Text>
+        );
+    }
+}
+
+export default AlbumContent;
---
+++ src/content/SongContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+class SongContent extends React.Component {
+    render() {
+        return (
+            <Text>Song</Text>
+        );
+    }
+}
+
+export default SongContent;
