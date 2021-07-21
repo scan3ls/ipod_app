a0065f18e66df1f31db5949a63617ff4ba462a40
scan3ls <scan3ls@gmail.com>
2021-07-20 22:14:15 -0500

    Create sorting function

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-function request(params) {
+export function request(params) {
     const base_url = 'https://salty-oasis-19252.herokuapp.com';
     // const base_url = 'http://127.0.0.1:5000/';
     const endpoints = ['albums', 'artists', 'songs'];
@@ -12,4 +12,27 @@ function request(params) {
     return promise;
 }
 
-export default request;
+export function sortedAlpha(items) {
+    const alpha = {};
+    const sorted = items.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+    'abcdefghijklmnopqrstuvwxyz'.split('').forEach(
+        letter => {
+            alpha[letter] = [] ;
+        }
+    );
+
+    sorted.forEach(
+        item => {
+            const letter = item.name[0].toLowerCase();
+            alpha[letter].push({
+                id: `${item.id}`,
+                name: `${item.name}`,
+                cover: `${item.cover}`
+            })
+        }
+    );
+
+    return alpha;
+}
+
